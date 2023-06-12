df671c5874ebbe1f1beec165003eab5bf0f9f081
Imon <contact@imonweb.co.uk>
2023-06-13 00:02:19 +0100

    home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
  
 
 function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hello World!</h1>
+      <Navbar />
+      <Home />
     </div>
   );
 }
